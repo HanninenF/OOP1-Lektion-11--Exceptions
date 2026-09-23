a38db13bc671081c29463df73e82364e9e93d66f
HanninenF <hanninenfredrik@gmail.com>
2026-09-23 11:42:02 +0200

    chore(project)Scaffold project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P01;
+
+internal enum PracticeTask
+{
+  FångaUndantag,
+}
+
+internal static class Program
+{
+  internal static void Main()
+  {
+    PracticeTask exercise = PracticeTask.FångaUndantag;
+    switch (exercise)
+    {
+      case PracticeTask.FångaUndantag:
+        break;
+    }
+  }
+}
